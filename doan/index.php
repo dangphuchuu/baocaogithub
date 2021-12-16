4870ad0581d113933e8b185be64458856d44f43e
dangphuchuu <92430448+dangphuchuu@users.noreply.github.com>
2021-12-16 13:58:09 +0700

    Delete doan directory

--- doan/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require './Controllers/BaseController.php';
-$controllerName = ucfirst(strtolower($_REQUEST['controller'] ?? 'Welcome') .'Controller');
-$actionName = $_REQUEST['action'] ?? 'index';
-require "./Controllers/${controllerName}.php";
-
-$controllerObject = new $controllerName;
-
-$controllerObject ->$actionName();
